0cd7ea9588474ee01b900dadf115d057ffc68d16
Gleb Kraitcer <kraitcer@gmail.com>
2023-07-02 10:48:51 +0400

    last but not least

--- src/components/NoteBadTimeAndSubmitAll.tsx
+++ src/components/NoteBadTimeAndSubmitAll.tsx
@@ -1,7 +1,10 @@
 import { Table, TableContainer, Tbody, Td, Tr } from "@chakra-ui/table";
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/tabs";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { ToDayGoals } from "./ToDayGoals";
+import SectionButton from "./UI Components/SectionButton";
+import { Input } from "@chakra-ui/input";
+import { Flex, Text } from "@chakra-ui/react";
 
 const NoteBadTimeAndSubmitAll = () => {
   const [projectsGoals, setProjectsGoals] = useState<any>();
@@ -9,98 +12,153 @@ const NoteBadTimeAndSubmitAll = () => {
   const [gratefulnessArray, setGratefulnessArray] = useState<any[]>([]);
   const [goalsAndEngineDataObj, setGoalsAndEngineDataObj] = useState<any[]>([]);
 
+  const tommorowGoals = [
+    { name: "goalOne", at: "goalOne_att_time_", iWill: "goalOne_IWill_Smart" },
+    { name: "goalTwo", at: "goalTwo_att_time_", iWill: "goalTwo_IWill_Smart" },
+    {
+      name: "goalThree",
+      at: "goalThree_att_time_",
+      iWill: "goalThree_IWill_Smart",
+    },
+  ];
+
+  const [tommorowGoalsArray, setTommorowGoalsArray] = useState<any[] | string>(
+    tommorowGoals
+  );
   const todayGoalsArray = [
     { name: "goalOneProgress" },
     { name: "goalThreeProgress" },
     { name: "goalTwoProgress" },
+    { name: "health" },
+    { name: "emotions" },
+    { name: "intellect" },
   ];
   const [todayGoals, setTodayGoals] = useState<any[]>(todayGoalsArray);
 
   useEffect(() => {
     const goalsAndEngineDataObj = localStorage.getItem("goalsAndEngineDataObj");
     if (goalsAndEngineDataObj)
-      setGoalsAndEngineDataObj(JSON.parse(goalsAndEngineDataObj));
+      return setGoalsAndEngineDataObj(JSON.parse(goalsAndEngineDataObj));
     const projectsGoals = localStorage.getItem("Tommorow Project & Goals");
-    if (projectsGoals) setProjectsGoals(JSON.parse(projectsGoals));
+    if (projectsGoals) return setProjectsGoals(JSON.parse(projectsGoals));
     const habitsArray = localStorage.getItem("Habits array");
-    if (habitsArray) setHabits(JSON.parse(habitsArray));
+    if (habitsArray) return setHabits(JSON.parse(habitsArray));
     const gratefulnessArray = localStorage.getItem("gratefulness array");
-    if (gratefulnessArray) setGratefulnessArray(JSON.parse(gratefulnessArray));
+    if (gratefulnessArray)
+      return setGratefulnessArray(JSON.parse(gratefulnessArray));
   }, []);
-  console.log(goalsAndEngineDataObj);
+  // console.log(goalsAndEngineDataObj);
   // console.log(gratefulnessArray);
   // console.log(projectsGoals);
   // console.log(habits);
 
   return (
-    <Tabs>
-      <TabList>
-        <Tab>Today</Tab>
-        <Tab>Gratefulness</Tab>
-        <Tab>VoteCast</Tab>
-        <Tab>Tommorow</Tab>
-      </TabList>
+    <>
+      <Tabs>
+        <TabList>
+          <Tab>Today</Tab>
+          <Tab>Gratefulness</Tab>
+          <Tab>VoteCast</Tab>
+          <Tab>Tommorow</Tab>
+        </TabList>
 
-      <TabPanels>
-        <TabPanel>
-          <TableContainer>
-            <Table variant="simple" h={"100%"}>
-              <Tbody>
-                {ToDayGoals?.map((goal, index) => (
-                  <Tr key={index} fontSize={17}>
-                    <Td ps={0} pb={2} pt={2} textAlign={"left"}>
-                      {goal}
-                    </Td>
-                    <Td pb={2} pt={2} pe={0} textAlign={"right"}>
-                      {goalsAndEngineDataObj[todayGoals[index].name]}
-                    </Td>
-                  </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </TabPanel>
-        <TabPanel>
-          <TableContainer>
-            <Table variant="simple" h={"100%"}>
-              <Tbody>
-                {gratefulnessArray?.map((grateElement, index) => (
-                  <Tr key={index} fontSize={17}>
-                    <Td ps={0} pb={2} pt={2} textAlign={"left"}>
-                      {grateElement?.gratefulForDoday}
-                    </Td>
-                    <Td pb={2} pt={2} pe={0} textAlign={"right"}>
-                      {grateElement?.grateCategories}
-                    </Td>
-                  </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </TabPanel>
-        <TabPanel>
-          <TableContainer>
-            <Table>
-              <Tbody>
-                {habits?.map((habit, index) => (
-                  <Tr key={index} fontSize={17}>
-                    <Td ps={0} pb={2} pt={2} textAlign={"left"}>
-                      {habit?.category}
-                    </Td>
-                    <Td pb={2} pt={2} pe={0} textAlign={"right"}>
-                      {`${habit?.habitImprovement}(${habit?.habitImprovementDetails})`}
-                    </Td>
-                  </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </TabPanel>
-        <TabPanel>
-          <p>three!</p>
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+        <TabPanels>
+          <TabPanel>
+            <TableContainer>
+              <Table variant="simple" h={"100%"}>
+                <Tbody>
+                  {ToDayGoals?.map((goal, index) => (
+                    <Tr key={index} fontSize={17}>
+                      <Td ps={0} pb={2} pt={2} textAlign={"left"}>
+                        {goal}
+                      </Td>
+                      <Td pb={2} pt={2} pe={0} textAlign={"right"}>
+                        {goalsAndEngineDataObj[todayGoals[index].name]}
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </TabPanel>
+          <TabPanel>
+            <TableContainer>
+              <Table variant="simple" h={"100%"}>
+                <Tbody>
+                  {gratefulnessArray?.map((grateElement, index) => (
+                    <Tr key={index} fontSize={17}>
+                      <Td ps={0} pb={2} pt={2} textAlign={"left"}>
+                        {grateElement?.gratefulForDoday}
+                      </Td>
+                      <Td pb={2} pt={2} pe={0} textAlign={"right"}>
+                        {grateElement?.grateCategories}
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </TabPanel>
+          <TabPanel>
+            <TableContainer>
+              <Table>
+                <Tbody>
+                  {habits?.map((habit, index) => (
+                    <Tr key={index} fontSize={17}>
+                      <Td ps={0} pb={2} pt={2} textAlign={"left"}>
+                        {habit?.category}
+                      </Td>
+                      <Td pb={2} pt={2} pe={0} textAlign={"right"}>
+                        {`${habit?.habitImprovement}(${habit?.habitImprovementDetails})`}
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </TabPanel>
+          <TabPanel>
+            <TableContainer>
+              <Table variant="simple" h={"100%"}>
+                <Tbody>
+                  {/* {tommorowGoals.map((goal, index) => (
+                    <Tr key={index} fontSize={17}>
+                      <Td ps={0} pb={2} pt={2} textAlign={"left"}>
+                        AT {projectsGoals[goal.at]}
+                      </Td>
+                      <Td pb={2} pt={2} pe={0} textAlign={"right"}>
+                        I WILL
+                      </Td>
+                      <Td pb={2} pt={2} pe={0} textAlign={"left"}>
+                        {projectsGoals[goal.iWill]}
+                      </Td>
+                    </Tr>
+                  ))} */}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+      <Flex mb={3} gap={3}>
+        <Text
+          w={250}
+          fontSize={24}
+          align={"center"}
+          textTransform={"uppercase"}
+          // justifyContent={"center"}
+          // alignItems={"center"}
+          flex={"nowrap"}
+        >
+          BAD TIME
+        </Text>
+        <Input type={"time"} w={60}></Input>
+        <SectionButton
+          onClick={() => console.log("totalSubmit")}
+          buttonName="Note Bad Time and Submit"
+        />
+      </Flex>
+    </>
   );
 };
 
--- src/components/ToDayGoals.ts
+++ src/components/ToDayGoals.ts
@@ -2,4 +2,7 @@ export const ToDayGoals = [
   "today Goal One",
   "today Goal Two",
   "today Goal Three",
+  "today Health",
+  "today Emotions",
+  "today Intellect",
 ] as const;
